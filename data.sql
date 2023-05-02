1d69b2ecc21f88730e21bf9b89d083939ee2768f
raihan2bd <raihan2bd.official@gmail.com>
2023-05-02 14:56:11 +0600

    Modify animals table owner_id column by owner name and animal name

--- data.sql
+++ data.sql
@@ -34,3 +34,15 @@ UPDATE animals
 
 UPDATE animals SET species_id = 1
   WHERE species_id IS NULL;
+
+-- Modify animals table owner_id column by owner name and animal name
+UPDATE animals SET owner_id = 1
+  WHERE name='Agumon';
+UPDATE animals SET owner_id = 2
+  WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3
+  WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4
+  WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5
+  WHERE name IN ('Angemon', 'Boarmon');
