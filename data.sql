3f7ef65c29c6bdff4b4f619bed7e5d3e1453e6a4
raihan2bd <raihan2bd.official@gmail.com>
2023-05-02 14:46:41 +0600

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -6,7 +6,6 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
     ('Devimon', '2017-05-12', 5, TRUE, 11);
 
 -- Insert more data into animals table
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Charmander', '2020-02-08', 0, false, -11),
     ('Plantmon', '2021-11-15', 2, true, -5.7),
@@ -15,3 +14,11 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
     ('Boarmon', '2005-06-07', 7, true, 20.4),
     ('Blossom', '1998-10-13', 3, true, 17),
     ('Ditto', '2022-05-14', 4, true, 22);
+
+-- Insert data into owners table
+INSERT INTO owners (full_name, age)
+  VALUES ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob',45), ('Melody Pond', 77), 
+    ('Dean Winchester', 14), 
+    ('Joddie Whittaker', 38);
