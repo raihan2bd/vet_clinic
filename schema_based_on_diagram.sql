1028867ca125d3493de94561d7223d5a58f235cf
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-05-09 11:30:28 +0100

    storing medical histories for patients

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,11 @@ CREATE TABLE patients (
   name VARCHAR(100),
   date_of_birth date
 );
+
+-- Stores medical histories for patients
+CREATE TABLE medical_histories(
+  id INT PRIMARY KEY Generated Always AS Identity,
+  admited_at TIMESTAMP,
+  patient_id INT CONSTRAINT fk_patient_id REFERENCES patients(id),
+  status VARCHAR(100)
+);
